54f119af03fec49af265b584bd32c8b264832cd5
CHBaker <charlesprobaker@mail.com>
2017-05-24 23:24:43 -0600

    BUG google not defined
    could be asynchronous error

--- js/script.js
+++ js/script.js
@@ -2,13 +2,106 @@ $(function () {
 
 	var places = [];
 
+    var mapstyle = [
+            {
+                "featureType": "landscape.natural",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    },
+                    {
+                        "color": "#e0efef"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    },
+                    {
+                        "hue": "#1900ff"
+                    },
+                    {
+                        "color": "#c0e8e8"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "lightness": 100
+                    },
+                    {
+                        "visibility": "simplified"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit.line",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    },
+                    {
+                        "lightness": 700
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "color": "#7dcdcd"
+                    }
+                ]
+            }
+        ];
+
 	var destination = function () {
 
 	};
 
-	var ViewModel = function () {
+    ko.bindingHandlers.googleMap = {
 
-	};
+        init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+            var mapObj = ko.utils.unwrapObservable(valueAccessor());
+            var latLng = new google.maps.LatLng(
+                ko.utils.unwrapObservable(mapObj.lat),
+                ko.utils.unwrapObservable(mapObj.lng));
+            var mapOptions = { center: latLng,
+                              zoom: 10, 
+                              styles: style,
+                              mapTypeControl: false };
+
+            mapObj.googleMap = new google.maps.Map(element, mapOptions);
+        }
+    };
+
+
+	var ViewModel = function () {
+        var self = this;
+        self.mainMap = ko.observable({
+            lat: ko.observable(38.907280),
+            lng: ko.observable(77.034488)
+        });
+    };
 
 	ko.applyBindings(new ViewModel());
 
