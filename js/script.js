b7e68484a73e4e9a49e3d270669bf78d46b46b3b
CHBaker <charlesprobaker@mail.com>
2017-07-03 17:10:25 -0600

    BUG FIX bind ko template

--- js/script.js
+++ js/script.js
@@ -198,41 +198,37 @@ var Place = function (locations, vm) {
     });
 
     populateInfoWindow = function (marker, infowindow) {
-    //     // make sure infowindow is not open already
-    //     if (infowindow.marker != marker) {
-    //         infowindow.setContent('');
-    //         infowindow.marker = marker;
-    //         infowindow.addListener('closeclick', function () {
-    //             infowindow.marker = null;
-    //         });
-    //         var streetViewService = new google.maps.StreetViewService();
-    //         var radius = 50;
-    //         // if Status OK, comput position of street view image
-    //         getStreetView = function (data, status) {
-    //             if (status == google.maps.StreetViewStatus.OK) {
-    //                 var nearStreetViewLocation = data.location.latLng;
-    //                 var heading = google.maps.geometry.spherical.computeHeading(
-    //                     nearStreetViewLocation, marker.position);
-    //                 var panoramaOptions = {
-    //                     position: nearStreetViewLocation,
-    //                     pov: {
-    //                         heading: heading,
-    //                         pitch: 30
-    //                     }
-    //                 };
-    //                 var panorama = new google.maps.StreetViewPanorama(
-    //                     document.getElementById('pano'), panoramaOptions);
-    //             } else {
-    //                 infowindow.setContent('<div>' + marker.title
-    //                     + '</div><div>No Street View Found</div>');
-    //             };
-    //         };
+        // make sure infowindow is not open already
+        infowindow.addListener('closeclick', function () {
+            infowindow.marker = null;
+        });
+        var streetViewService = new google.maps.StreetViewService();
+        var radius = 50;
+        // if Status OK, comput position of street view image
+        getStreetView = function (data, status) {
+            if (status == google.maps.StreetViewStatus.OK) {
+                var nearStreetViewLocation = data.location.latLng;
+                var heading = google.maps.geometry.spherical.computeHeading(
+                    nearStreetViewLocation, marker.position);
+                var panoramaOptions = {
+                    position: nearStreetViewLocation,
+                    pov: {
+                        heading: heading,
+                        pitch: 30
+                    }
+                };
+                var panorama = new google.maps.StreetViewPanorama(
+                    document.getElementById('pano'), panoramaOptions);
+            } else {
+                infowindow.setContent('<div>' + marker.title
+                    + '</div><div>No Street View Found</div>');
+            };
+        };
 
-    //         // get the closest streetview image within 50 meters of marker
-    //         streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
-    //         // Open infowindow on the selected marker
-             infowindow.open(map, marker);
-    //     };
+        // get the closest streetview image within 50 meters of marker
+        streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+        // Open infowindow on the selected marker
+         infowindow.open(map, marker);
     };
 };
 
@@ -262,7 +258,7 @@ var ViewModel = function () {
 
         google.maps.event.addListener(infoWindow, 'domready', function () {
             if (!infoWindowLoaded) {
-                ko.applyBindings(self, document.getElementById('info-template')[0]);
+                ko.applyBindings(self, document.getElementById('info-window'));
                 infoWindowLoaded = true;
             }
         });
@@ -311,7 +307,8 @@ var ViewModel = function () {
     this.showWiki = ko.observable(false);
 
     this.toggleWiki = function () {
-        this.showWiki(!this.showWiki());
+        // this.showWiki(!this.showWiki());
+        console.log('wiki!' + self.currentLocation().title);
     };
 
     // trigger marker click, when list item is clicked
